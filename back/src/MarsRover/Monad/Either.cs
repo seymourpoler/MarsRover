f4c22f870c4f403b559bf1eacd5e23451f1250a5
Luis Rodríguez <luis.rodriguez@leanmind.es>
2023-12-01 16:19:21 +0100

    Monad Either Match

---
+++ back/src/MarsRover/Monad/Either.cs
@@ -0,0 +1,43 @@
+﻿namespace MarsRover.Monad
+{
+    public readonly struct Either<TError, TSuccess>
+    {
+        private readonly TError ErrorValue;
+        private readonly TSuccess SuccessValue;
+
+        private Either(TError errorValue)
+        {
+            ErrorValue = errorValue;
+            SuccessValue = default;
+            IsSuccess = false;
+            IsError = true;
+        }
+
+        private Either(TSuccess successValue)
+        {
+            ErrorValue = default;
+            SuccessValue = successValue;
+            IsSuccess = true;
+            IsError = false;
+        }
+
+        public static Either<TError, TSuccess> Error(TError error) => new(error);
+
+        public static Either<TError, TSuccess> Success(TSuccess success) => new(success);
+
+        public bool IsSuccess { get; }
+
+        public bool IsError { get; }
+
+        public TResult Match<TResult>(Func<TError, TResult> errorFunc, Func<TSuccess, TResult> successFunc) =>
+            IsSuccess
+                ? successFunc(SuccessValue)
+                : errorFunc(ErrorValue);
+
+        public Task<TResult> MatchAsync<TResult>(Func<TError, TResult> errorFunc, Func<TSuccess, Task<TResult>> successFunc) =>
+            IsSuccess
+                ? successFunc(SuccessValue)
+                : Task.FromResult(errorFunc(ErrorValue));
+
+    }
+}
---
+++ back/tests/MarsRovers.Unit.Tests/Monad/WhenMatching.cs
@@ -0,0 +1,33 @@
+﻿using MarsRover.Monad;
+
+namespace MarsRovers.Unit.Tests.Monad
+{
+    public class WhenMatching
+    {
+        [Fact]
+        public void MatchWithTheErrorSide()
+        {
+            var eitherWithAnError = Either<Error, Success>.Error(new Error());
+
+            eitherWithAnError.Match(
+                errorFunc: error => error.Should().BeOfType<Error>(),
+                successFunc: _ => throw new Exception("Either fails"));
+            
+
+        }
+
+        [Fact]
+        public void MatchWithTheSuccessSide()
+        {
+            var eitherWithAnError = Either<Error, Success>.Success(new Success());
+
+            eitherWithAnError.Match(
+                successFunc: error => error.Should().BeOfType<Success>(),
+                errorFunc: _ => throw new Exception("Either fails"));
+
+        }
+
+        private class Error { }
+        public class Success { }
+    }
+}
