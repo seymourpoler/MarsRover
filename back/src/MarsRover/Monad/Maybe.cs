6bf82e1c01074e86155ad2dcfdbe395169a1f132
Luis Rodríguez <luis.rodriguez@leanmind.es>
2023-12-01 17:57:54 +0100

    monad maybe match

---
+++ back/src/MarsRover/Monad/Maybe.cs
@@ -0,0 +1,23 @@
+﻿namespace MarsRover.Monad
+{
+    public struct Maybe<T>
+    {
+        private readonly T value;
+
+        private Maybe(T value)
+        {
+            this.value = value;
+        }
+
+        public static Maybe<T> Nothing() => new Maybe<T>();
+
+        public static Maybe<T> Just(T value) => new Maybe<T>(value);
+
+        public TResult Match<TResult>(Func<TResult> nothing, Func<T, TResult> just)
+        {
+            return value is null 
+                ? nothing()
+                : just(value);
+        }
+    }
+}
---
+++ back/tests/MarsRovers.Unit.Tests/Monad/WhenMaybeMatching.cs
@@ -0,0 +1,34 @@
+﻿using MarsRover.Monad;
+
+namespace MarsRovers.Unit.Tests.Monad
+{
+    public class WhenMaybeMatching
+    {
+        [Fact]
+        public void MatchWithTheErrorSide()
+        {
+            var manybeNothing = Maybe<Thing>.Nothing();
+
+            var result = manybeNothing.Match<string>(
+                nothing: () => { return "nothing"; },
+                just : _ => throw new Exception("Maybe with something"));
+
+            result.Should().Be("nothing");
+        }
+
+        [Fact]
+        public void MatchWithTheSuccessSide()
+        {
+            var maybeWithSomething = Maybe<Thing>.Just(new Thing());
+
+            var result = maybeWithSomething.Match(
+                just: something => { return "something"; },
+                nothing: () => throw new Exception("Either fails"));
+
+            result.Should().Be("something");
+
+        }
+
+        private class Thing { }
+    }
+}
